8e620aeba64c982f39d10fd033038e17f092b669
ajay <ajaysr047@gmail.com>
2021-01-06 21:14:58 +0530

    second commit

--- Dummy.java
+++ Dummy.java
@@ -3,5 +3,6 @@ public class Dummy
     public static void main(String args[])
     {
         System.out.println("Dummy Java Class!");
+        System.out.println("Print to highlight git diff");
     }
 }
